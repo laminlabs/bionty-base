2b0e5895f0b3960234b3aef5299296590936ac67
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-05-29 17:04:20 +0800

    :zap: fetch species table using Ensembl REST api instead

--- bionty/taxon/_query.py
+++ bionty/taxon/_query.py
@@ -1,8 +1,52 @@
-from biothings_client import get_client
+import sys
+
+import pandas as pd
+import requests  # type: ignore
+import xmltodict
+
+from .._urls import ENSEMBL_REST, ENSEMBL_REST_EXT
 
 
 class Mytaxon:
     """Wrapper of MyTaxon.info."""
 
     def __init__(self) -> None:
+        from biothings_client import get_client
+
         self.sever = get_client("taxon", instance=False)
+
+
+def species_table() -> pd.DataFrame:
+    """Fetch species table from Ensembl REST.
+
+    Returns:
+        a dataframe
+    """
+    server = ENSEMBL_REST
+    ext = ENSEMBL_REST_EXT.SPECIES_INO
+
+    r = requests.get(server + ext, headers={"Content-Type": "text/xml"})
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+
+    # format into a dataframe
+    entries = xmltodict.parse(r.text)["opt"]["data"]["species"]
+    sp_dict: dict = {}
+    cols = [
+        "display_name",
+        "common_name",
+        "taxon_id",
+        "assembly",
+        "accession",
+        "release",
+    ]
+    for entry in entries:
+        id = entry.get("@name")
+        sp_dict[id] = []
+        for i in cols:
+            sp_dict[id].append(entry.get(f"@{i}"))
+    sp_df = pd.DataFrame.from_dict(sp_dict).T
+    sp_df.columns = cols
+
+    return sp_df
