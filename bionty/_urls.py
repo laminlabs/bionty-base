95b87f5164663fdea0e407ed6002af69b362f774
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-05-30 16:42:11 +0800

    :memo: updated ensembl notes

--- bionty/_urls.py
+++ bionty/_urls.py
@@ -1,5 +1,10 @@
 from argparse import Namespace
 
 # Ensembl REST server
-ENSEMBL_REST = "http://rest.ensembl.org"
-ENSEMBL_REST_EXT = Namespace(SPECIES_INFO="/info/species?")
+ENSEMBL_REST = "http://rest.ensembl.org/"
+ENSEMBL_REST_EXT = Namespace(SPECIES_INFO="info/species?")
+
+# HGNC REST server
+"""See searchable fields here: https://www.genenames.org/help/rest/"""
+HGNC_REST = "http://rest.genenames.org/"
+HGNC_REST_EXT = Namespace(INFO="info", FETCH="fetch/", SEARCH="search/")
