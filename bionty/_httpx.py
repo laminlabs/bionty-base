0e53721b6e65929f0f15f03ffd47a3f8fe340b0e
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-06-12 22:57:34 +0800

    :zap: switched to async get requests

--- bionty/_httpx.py
+++ bionty/_httpx.py
@@ -1,19 +1,55 @@
+import asyncio
+import sys
 from typing import Any
 
 import httpx
+import nest_asyncio
+import requests  # type: ignore
+from syncer import sync
+
+nest_asyncio.apply()  # Fixes the issue with iPython compatibility
 
 
-async def async_get(client, url):
-    resp = await client.get(url)
-    return resp
+async def _async_get(client, url, content_type="application/json"):
+    resp = await client.get(url, headers={"content-type": content_type})
+
+    if resp.headers["content-type"].find("application/json") > -1:
+        return resp.json()
+    else:
+        return resp.text
 
 
-def fetch_endpoint(server, request, content_type="application/json", **kwds) -> Any:
+@sync
+async def get_request_async(
+    server_request: str, terms, content_type="application/json"
+):
+    async with httpx.AsyncClient() as client:
+
+        tasks = []
+
+        for term in terms:
+            url = f"{server_request}{term}"
+            tasks.append(
+                asyncio.ensure_future(
+                    _async_get(client, url, content_type=content_type)
+                )
+            )
+
+        resps = await asyncio.gather(*tasks)
+
+    return resps
+
+
+def get_request(server, request, content_type="application/json", **kwds) -> Any:
     """Fetch an endpoint from the server.
 
     Allow overriding of default content-type
     """
-    r = httpx.get(server + request, headers={"Accept": content_type}, **kwds)
+    r = requests.get(server + request, headers={"Accept": content_type}, **kwds)
+
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
 
     if content_type == "application/json":
         return r.json()
@@ -21,13 +57,15 @@ def fetch_endpoint(server, request, content_type="application/json", **kwds) -> 
         return r.text
 
 
-def fetch_endpoint_POST(
-    server, request, data, content_type="application/json", **kwds
-) -> Any:
+def post_request(server, request, data, content_type="application/json", **kwds) -> Any:
     """POST requests."""
-    r = httpx.post(
+    r = requests.post(
         server + request, headers={"Content-Type": content_type}, data=data, **kwds
     )
+
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
 
     if content_type == "application/json":
         return r.json()
