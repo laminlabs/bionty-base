6c495085804ffe42a9c49c71c13bb6c7c2a7c6c4
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-06-23 01:06:46 +0800

    :sparkles: Also implemented tissue dataclass

--- bionty/tissue/_core.py
+++ bionty/tissue/_core.py
@@ -1,5 +1,12 @@
+from functools import cached_property
+
+from .._io import loads_pickle
+from .._models import create_model
 from .._ontology import Ontology
+from .._settings import check_dynamicdir_exists, settings
 from .._urls import OBO_UBERON_OWL
+
+TissueData = create_model("TissueData", __module__=__name__)
 
 
 class Tissue(Ontology):
@@ -9,5 +16,39 @@ class Tissue(Ontology):
     https://github.com/obophenotype/uberon
     """
 
-    def __init__(self, base_iri=OBO_UBERON_OWL, load: bool = True) -> None:
-        super().__init__(base_iri=base_iri, load=load)
+    def __init__(self) -> None:
+        self._dataclasspath = settings.dynamicdir / "tissuedataclass.pkl"
+        if not self.dataclasspath.exists():
+            super().__init__(base_iri=OBO_UBERON_OWL, load=True)
+
+    @property
+    def dataclasspath(self):
+        """Path to the picked dataclass."""
+        return self._dataclasspath
+
+    @cached_property
+    def onto_dict(self) -> dict:
+        """Keyed by name, valued by label."""
+        return {
+            v.name: v.label[0]
+            for k, v in self.classes.items()
+            if k.startswith("CL") & len(v.label) > 0
+        }
+
+    @cached_property
+    def dataclass(self):
+        """Pydantic dataclass of tissues."""
+        return self._load_dataclass()
+
+    @check_dynamicdir_exists
+    def _load_dataclass(self):
+        """Loading dataclass from the pickle file."""
+        if not self.dataclasspath.exists():
+            import pickle
+
+            from .._io import write_pickle
+
+            TissueData.add_fields(**self.onto_dict)
+            write_pickle(pickle.dumps(TissueData()), self.dataclasspath)
+
+        return loads_pickle(self.dataclasspath)
