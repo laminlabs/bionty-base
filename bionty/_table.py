418a294a4d74e20893cb72c178e6e207a4cffd2d
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-28 08:44:08 +0200

    Clarify all different possible table representations

--- bionty/_table.py
+++ bionty/_table.py
@@ -1,4 +1,13 @@
 import pandas as pd
+from typing import Protocol
+from pydantic import BaseModel
+from dataclasses as dataclass
+
+
+class DataClass(Protocol):
+    # as already noted in comments, checking for this attribute is currently
+    # the most reliable way to ascertain that something is a dataclass
+    __dataclass_fields__: Dict
 
 
 class Table:
@@ -9,5 +18,20 @@ class Table:
 
     @cached_property
     def df(self) -> pd.DataFrame:
-        """DataFrame."""
+        """DataFrame represenation of table."""
+        raise NotImplementedError
+
+    @cached_property
+    def dc(self) -> DataClass:
+        """Dataclass representation of table."""
+        raise NotImplementedError
+
+    @cached_property
+    def bm(self) -> BaseModel:
+        """BaseModel constructed from dataclass."""
+        raise NotImplementedError
+    
+    @cached_property    
+    def sqlm(self):
+        """SQLModel constructed from BaseModel."""
         raise NotImplementedError
