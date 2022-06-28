916f2eb5000f6ba46b30344619fec14b0ffd7f76
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-28 14:57:23 +0200

    :truck: Move IO to assets

--- bionty/_io.py
+++ bionty/_io.py
@@ -1,4 +1,3 @@
-import pickle
 from pathlib import Path
 from typing import Union
 
@@ -7,30 +6,6 @@ from typeguard import typechecked
 
 
 @typechecked
-def read_pickle(filename: Union[str, Path]):
-    with open(filename, "rb") as handle:
-        return pickle.load(handle)
-
-
-def loads_pickle(filename: Union[str, Path]):
-    with open(filename, "rb") as handle:
-        r = pickle.load(handle)
-    return pickle.loads(r)
-
-
-@typechecked
-def write_pickle(data, filename: Union[str, Path]) -> None:
-    with open(filename, "wb") as handle:
-        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-@typechecked
 def read_json(filename: Union[str, Path]):
     with open(filename, "rb") as f:
         return orjson.loads(f.read())
-
-
-@typechecked
-def write_json(data, filename: Union[str, Path]):
-    with open(filename, "wb") as f:
-        f.write(orjson.dumps(data))
