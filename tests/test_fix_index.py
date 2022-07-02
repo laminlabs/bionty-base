26db642a5a8cf002ba4ac870c7cbc53b444b4d9b
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-02 20:28:34 +0200

    :lipstick: Prettify dataframe generation

--- tests/test_fix_index.py
+++ tests/test_fix_index.py
@@ -3,18 +3,17 @@ import pytest
 
 from bionty import get_compliant_index_from_column
 
-df = pd.DataFrame(
-    {
-        "hgnc_symbol": ["A1CF", "A1BG", "corrupted1", "corrupted2"],
-        "hgnc_id": ["HGNC:24086", "HGNC:5", "corrupted1", "corrupted2"],
-        "ensembl.gene_id": [
-            "ENSG00000148584",
-            "ENSG00000121410",
-            "corrupted1",
-            "corrupted2",
-        ],
-    }
-).set_index("hgnc_id")
+data = {
+    "hgnc_symbol": ["A1CF", "A1BG", "corrupted1", "corrupted2"],
+    "hgnc_id": ["HGNC:24086", "HGNC:5", "corrupted1", "corrupted2"],
+    "ensembl.gene_id": [
+        "ENSG00000148584",
+        "ENSG00000121410",
+        "corrupted1",
+        "corrupted2",
+    ],
+}
+df = pd.DataFrame(data).set_index("hgnc_id")
 
 
 def test_get_compliant_index_from_column():
--- tests/test_table.py
+++ tests/test_table.py
@@ -2,18 +2,17 @@ import pandas as pd
 
 from bionty import Gene
 
-df = pd.DataFrame(
-    {
-        "hgnc_symbol": ["A1CF", "A1BG", "corrupted1", "corrupted2"],
-        "hgnc_id": ["HGNC:24086", "HGNC:5", "corrupted1", "corrupted2"],
-        "ensembl.gene_id": [
-            "ENSG00000148584",
-            "ENSG00000121410",
-            "corrupted1",
-            "corrupted2",
-        ],
-    }
-).set_index("hgnc_id")
+data = {
+    "hgnc_symbol": ["A1CF", "A1BG", "corrupted1", "corrupted2"],
+    "hgnc_id": ["HGNC:24086", "HGNC:5", "corrupted1", "corrupted2"],
+    "ensembl.gene_id": [
+        "ENSG00000148584",
+        "ENSG00000121410",
+        "corrupted1",
+        "corrupted2",
+    ],
+}
+df = pd.DataFrame(data).set_index("hgnc_id")
 
 
 def test_curate():
