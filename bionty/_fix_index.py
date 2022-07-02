3b3c6f06271c847b07af57d372f91e66b02dd661
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-02 18:17:51 +0200

    :recycle: Refactor Table.curate

--- bionty/_fix_index.py
+++ bionty/_fix_index.py
@@ -1,11 +1,10 @@
 from typing import Iterable
 
+import numpy as np
 import pandas as pd
 
-from ._logging import logger as logg
 
-
-def check_if_index_compliant(index: Iterable, ref_index: Iterable):
+def check_if_index_compliant(index: Iterable, ref_index: Iterable) -> np.ndarray:
     """The index already theoretically conforms with the Bionty ID for the entity.
 
     Meaning, the name of the index column is, for instance `hgnc_symbol`.
@@ -20,14 +19,10 @@ def check_if_index_compliant(index: Iterable, ref_index: Iterable):
     else:
         input_index = index  # type: ignore
 
-    matches = input_index.isin(
-        ref_index
-    )  # boolean vector indicating standardized terms
+    # boolean vector indicating standardized terms
+    matches = input_index.isin(ref_index)
 
-    if all(matches) is True:
-        return True
-    else:
-        return input_index[~matches]
+    return matches
 
 
 def get_compliant_index_from_column(
@@ -60,48 +55,29 @@ def get_compliant_index_from_column(
     """
     df = df.copy()  # not touch the input df
 
-    if column not in ref_df.reset_index().columns:
-        raise AssertionError(
-            f"{column} name must match one of {ref_df.reset_index().columns}!"
-        )
+    # prepare ref_df for lookup
+    ref_index = ref_df.index
+    ref_df = ref_df.reset_index()
 
-    query_values = df[column].values
+    if column not in ref_df.columns:
+        raise AssertionError(f"{column} name must match one of {ref_df.columns}!")
+    else:
+        query_values = df[column].values
 
     # lookup_df is indexed with the query column field in the ref_df
-    # and with a "bionty_id" column containing the primary bionty gene id
+    # and with a "bionty_id" column containing the Bionty default identifier
     # e.g. default is `hgnc_symbol` for human
-    lookup_df = pd.DataFrame(
-        index=ref_df.reset_index()[column], data={"bionty_id": ref_df.index}
-    )
+    lookup_df = pd.DataFrame(index=ref_df[column], data={"bionty_id": ref_index})
 
-    # this will fail if there are typos
-    # need to think about warning flags and soft implementations of this
-    unmapped = check_if_index_compliant(query_values, lookup_df.index)
+    matches = check_if_index_compliant(query_values, lookup_df.index)
 
-    if isinstance(unmapped, bool):
-        # everything is mappable
-        mapped_index = query_values
-        integrity = 1.0
-    elif len(unmapped) == len(df[column]):
-        # not mappable
-        mapped_index = pd.Index(  # type:ignore
-            []
-        )  # smart semi-compliant index definition
-        integrity = 0.0
-        logg.warning("The input column is not mappable to the bionty reference!")
-    else:
-        # partially mappable
-        # number_of_mappable_compliant_terms/total_number_of_terms
-        mapped_index = pd.Index(query_values).difference(unmapped)
-        integrity = round((1 - len(unmapped) / len(df[column])) * 100, 2)
-        logg.warning(f"Only {integrity} of terms are mappable!")
+    if all(~matches):  # nothing is mappable
+        raise AssertionError(f"{column} name must contain at least one mappable term!")
 
-    # mapped_dict is the {query_id: bionty_id}
-    mapped_dict = lookup_df.loc[mapped_index].to_dict()["bionty_id"]
-    new_index = df[column].map(mapped_dict)
+    # mapped_dict is the {query_id: bionty_id}, where query_id = ref_df[column]
+    lookup_df_mappable = lookup_df.loc[query_values[matches]]
+    mapper = lookup_df_mappable["bionty_id"]
+    new_index = df[column].map(mapper)  # what's unmappable results in nan
+    new_index = new_index.fillna(df[column]).values  # fill nans with orig values
 
-    if keep_data:
-        # the unmapped terms will be kept as they are in the index
-        new_index = new_index.fillna(df[column]).values
-
-    return pd.Index(new_index), integrity
+    return pd.Index(new_index), matches
