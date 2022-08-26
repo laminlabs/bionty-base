05d973d66421b21bdb31f597c2e2a21a8828b3a9
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-26 19:45:25 +0200

    :bento: Updated protein tables

--- bionty/_normalize.py
+++ bionty/_normalize.py
@@ -13,14 +13,27 @@ GENE_COLUMNS = {
     "MGI ID": "mgi_id",
 }
 
+PROTEIN_COLUMNS = {
+    "Entry": "uniprotkb_id",
+    "Entry Name": "uniprotkb_name",
+    "Protein names": "protein_names",
+    "Length": "length",
+    "Gene Names (primary)": "gene_symbols",
+    "Gene Names (synonym)": "gene_synonyms",
+    "Ensembl": "ensembl_transcript_ids",
+    "GeneID": "ncbi_gene_ids",
+}
+
 
 class NormalizeColumns:
     """Standardizing column names."""
-
-    def __init__(self) -> None:
-        pass
 
     @staticmethod
     def gene(df: pd.DataFrame, species=None):
         """Column names of gene EntityTables."""
         df.rename(columns=GENE_COLUMNS, inplace=True)
+
+    @staticmethod
+    def protein(df: pd.DataFrame, species=None):
+        """Column names of protein EntityTables."""
+        df.rename(columns=PROTEIN_COLUMNS, inplace=True)
--- bionty/_protein/_core.py
+++ bionty/_protein/_core.py
@@ -2,7 +2,31 @@ from functools import cached_property
 
 import pandas as pd
 
+from .._normalize import NormalizeColumns
 from .._settings import check_datasetdir_exists, settings
+
+
+def _get_shortest_name(df, column):
+    """Get a single shortest name from a column of lists."""
+    name_list = []
+    names_list = []
+    for i in df[column]:
+        i = i.replace(", ", "|")
+        names_list.append(i)
+
+        def shortest_name(lst: list):
+            return min(lst, key=len)
+
+        names = i.split("|")
+        no_space_names = [i for i in names if " " not in i]
+        if len(no_space_names) == 0:
+            name = shortest_name(names)
+        else:
+            name = shortest_name(no_space_names)
+        name_list.append(name)
+
+    df[column.rstrip("s")] = name_list
+    df[column] = names_list
 
 
 class Protein:
@@ -12,9 +36,10 @@ class Protein:
         species: `common_name` of `Species` entity EntityTable.
     """
 
-    def __init__(self, species="human") -> None:
+    def __init__(self, species="human", id=None) -> None:
         self._species = species
-        self._filepath = settings.datasetdir / f"{self.species}-uniprot.feather"
+        self._filepath = settings.datasetdir / f"uniprot-{self.species}.feather"
+        self._id_field = "uniprotkb_id" if id is None else id
 
     @property
     def entity(self):
@@ -39,13 +64,16 @@ class Protein:
         else:
             if not self.filepath.exists():
                 self._download_df()
-            return pd.read_feather(self.filepath).set_index("UniProtKB-AC")
+            df = pd.read_feather(self.filepath)
+            NormalizeColumns.protein(df)
+            _get_shortest_name(df, "protein_names")
+            return df.set_index(self._id_field)
 
     @check_datasetdir_exists
     def _download_df(self):
         from urllib.request import urlretrieve
 
         urlretrieve(
-            f"https://bionty-assets.s3.amazonaws.com/{self.species}-uniprot.feather",
+            f"https://bionty-assets.s3.amazonaws.com/uniprot-{self.species}.feather",
             self.filepath,
         )
