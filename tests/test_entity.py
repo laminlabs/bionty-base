4f0ef5bbc4cfdb04ff5eccc5a76f950e4887bbb4
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-27 11:50:26 +0200

    :white_check_mark: simplified tests

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -20,42 +20,20 @@ def test_entity_table():
         entity_table.ontology
 
 
-def test_cell_marker():
+def species_not_implemented():
     with pytest.raises(NotImplementedError):
         CellMarker(species="mouse")
-    cell_marker = CellMarker()
-    assert cell_marker.entity == "cell_marker"
-
-
-def test_protein():
     with pytest.raises(NotImplementedError):
         Protein(species="cat")
-    protein = Protein()
-    assert protein.entity == "protein"
-
-
-def test_gene():
     with pytest.raises(NotImplementedError):
         Gene(species="cat")
-    gene = Gene()
-    assert gene.entity == "gene"
 
 
-def test_cell_type():
-    cell_type = CellType()
-    assert cell_type.entity == "cell_type"
-
-
-def test_species():
-    species = Species()
-    assert species.entity == "species"
-
-
-def test_tissue():
-    tissue = Tissue()
-    assert tissue.entity == "tissue"
-
-
-def test_disease():
-    disease = Disease()
-    assert disease.entity == "disease"
+def test_entity():
+    assert Gene().entity == "gene"
+    assert Protein().entity == "protein"
+    assert CellMarker().entity == "cell_marker"
+    assert Species().entity == "species"
+    assert Tissue().entity == "tissue"
+    assert CellType().entity == "cell_type"
+    assert Disease().entity == "disease"
