d1d461adccda682f10833aa3b3c4bcf937debc4d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-27 11:27:28 +0200

    :rewind: updated imports from dev

--- bionty/_table.py
+++ bionty/_table.py
@@ -5,7 +5,7 @@ from functools import cached_property
 import pandas as pd
 
 from ._logger import logger
-from .dev import (
+from .dev._fix_index import (
     check_if_index_compliant,
     explode_aggregated_column_to_expand,
     get_compliant_index_from_column,
