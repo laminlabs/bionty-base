0e3e47104cefb0ec3cd243af1c116f5c1b36b9d0
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-28 12:03:57 +0200

    :art: fixed species

--- bionty/_cellmarker/_core.py
+++ bionty/_cellmarker/_core.py
@@ -16,9 +16,9 @@ class CellMarker(EntityTable):
 
     def __init__(self, species="human", id=None) -> None:
         super().__init__(id=id)
-        self._species = species
-        if self.species not in {"human"}:
+        if species not in {"human"}:
             raise NotImplementedError
+        self._species = species
         self._filepath = settings.datasetdir / f"CellMarker-{self.species}.feather"
         self._id_field = "cell_marker" if id is None else id
 
