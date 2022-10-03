ee5625f06874b2587c32be9af17b705e8f1b5fa6
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-10-03 18:32:27 +0200

    :bug: Fix duplicated cell marker names

--- bionty/_cellmarker/_core.py
+++ bionty/_cellmarker/_core.py
@@ -43,6 +43,8 @@ class CellMarker(EntityTable):
         df = pd.read_parquet(self._filepath)
         df.rename(columns={"cell_marker": "name"}, inplace=True)
         df["name"] = df["name"].str.upper()
+        # TODO: add to bionty-assets
+        df = df.drop_duplicates(subset=["name"])
         if not df.index.is_numeric():
             df = df.reset_index().copy()
         df = df[~df[self._id_field].isnull()]
