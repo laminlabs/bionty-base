f60bb10a60937c5380e4da2ae861aaf5028154a4
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-10-20 14:18:51 +0200

    :fire: Removed feature_model lookups

--- bionty/_lookup.py
+++ bionty/_lookup.py
@@ -4,8 +4,6 @@ from typing import Iterable
 from ._normalize import GENE_COLUMNS, PROTEIN_COLUMNS
 from ._species import Species
 
-FEATURES = ["gene", "protein", "cell_marker"]
-
 
 def _lookup(values: Iterable[str]):
     """Look up a list of values via tab completion."""
@@ -16,7 +14,6 @@ def _lookup(values: Iterable[str]):
 class lookup:
     """Look up a list of values via tab completion."""
 
-    feature_model = _lookup(values=FEATURES)
     gene_id = _lookup(values=set(GENE_COLUMNS.values()))
     protein_id = _lookup(values=set(PROTEIN_COLUMNS.values()))
     species = Species().lookup
