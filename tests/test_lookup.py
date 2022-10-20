5a6dbc93bd3b895011b719b42a7e6f5bd9125b34
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-10-20 14:27:39 +0200

    :green_heart: fix tests

--- tests/test_lookup.py
+++ tests/test_lookup.py
@@ -4,5 +4,4 @@ from bionty import lookup
 def test_lookup():
     assert lookup.gene_id.ensembl_gene_id == "ensembl_gene_id"
     assert lookup.protein_id.uniprotkb_id == "uniprotkb_id"
-    assert lookup.feature_model.gene == "gene"
     assert lookup.species.mouse == "mouse"
