a064cd2f581f1c8e2d5307123d181ac00d673272
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-22 14:35:45 +0800

    :art: read ontology url from yaml

--- bionty/dev/_io.py
+++ bionty/dev/_io.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+from typing import Union
+
+import requests  # type:ignore
 import yaml  # type:ignore
 
 
-def load_yaml(filename: str):
+def load_yaml(filename: Union[str, Path]):
     with open(filename, "r") as f:
         return yaml.safe_load(f)
 
@@ -9,3 +13,14 @@ def load_yaml(filename: str):
 def write_yaml(data: dict, filename: str):
     with open(filename, "w") as f:
         yaml.dump(data, f)
+
+
+def url_download(url: str, filename: str = None, **kwargs):
+    r = requests.get(url, allow_redirects=True, **kwargs)
+    if r.status_code != 200:
+        raise ConnectionError(f"could not download {url}\nerror code: {r.status_code}")
+
+    if filename is None:
+        filename = url.split("/")[-1]
+
+    Path(filename).write_bytes(r.content)
