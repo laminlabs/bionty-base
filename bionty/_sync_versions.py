30bb0ad95336ce05237684727a0c831017956fdd
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-23 21:15:07 +0800

    :green_heart: path

--- bionty/_sync_versions.py
+++ bionty/_sync_versions.py
@@ -2,15 +2,17 @@ from pathlib import Path
 
 from .dev._io import load_yaml, write_yaml
 
-_VERSIONS_PATH = Path(__file__).parent / "_versions.yaml"
+ROOT = Path(__file__).parent / "versions"
+VERSIONS = ROOT / "versions.yaml"
+_VERSIONS = ROOT / "_versions.yaml"
 
 # writes the most recent version to the _versions.yaml
-if not _VERSIONS_PATH.exists():
-    versions = load_yaml("versions/versions.yaml")
+if not _VERSIONS.exists():
+    versions = load_yaml(VERSIONS)
     _versions = {}
     for name, db_versions in versions.items():
         db = next(iter(db_versions))
         versions = db_versions.get(db).get("versions")
         version = sorted(versions.keys(), reverse=True)[0]
         _versions[name] = {db: version}
-    write_yaml(_versions, _VERSIONS_PATH)
+    write_yaml(_versions, _VERSIONS)
