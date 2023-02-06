ff03d02f06ffaeb902c07203947f341cdc927d2e
zethson <lukas.heumos@posteo.net>
2023-02-06 13:39:01 +0100

    Add species to hpo
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- bionty/_phenotype/_core.py
+++ bionty/_phenotype/_core.py
@@ -7,9 +7,16 @@ from .._ontology import Ontology
 from .._settings import settings
 from .._table import EntityTable
 
+FILENAMES = {
+    "human": "phenotype_lookup.parquet",
+}
+
 
 class Phenotype(EntityTable):
     """Phenotype.
+
+    Args:
+        species: `name` of `Species` entity EntityTable.
 
     Edits of terms are coordinated and reviewed on:
     https://hpo.jax.org/app/
@@ -17,17 +24,26 @@ class Phenotype(EntityTable):
 
     def __init__(
         self,
+        species: str = "human",
         id: str = "ontology_id",
         database: Optional[str] = None,
         version: Optional[str] = None,
     ) -> None:
-        database = "hpo" if database is None else database
+        database = "hp" if database is None else database
         super().__init__(id=id, database=database, version=version)
+        if FILENAMES.get(species) is None:
+            raise NotImplementedError
+        self._species = species
+
+    @property
+    def species(self):
+        """The `name` of `Species` entity EntityTable."""
+        return self._species
 
     @cached_property
     def df(self) -> pd.DataFrame:
         """DataFrame."""
-        self._filepath = settings.datasetdir / "phenotype_lookup.parquet"
+        self._filepath = settings.datasetdir / FILENAMES.get(self.species)
 
         if not self._filepath.exists():
             df = self._ontology_to_df(self.ontology)
