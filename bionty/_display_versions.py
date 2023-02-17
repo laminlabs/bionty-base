8695a343fbd2c47bea0ddcdf4972e6251be07609
zethson <lukas.heumos@posteo.net>
2023-02-17 14:19:56 +0100

    :sparkles: Add display tables
    Signed-off-by: zethson <lukas.heumos@posteo.net>

---
+++ bionty/_display_versions.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+from typing import Literal
+
+from rich.console import Console
+from rich.table import Table
+
+from bionty.dev._io import load_yaml
+
+console = Console()
+
+
+def display_available_versions() -> None:  # pragma: no cover
+    """Displays all available entities and versions in a Rich table."""
+    VERSIONS_FILE_PATH = Path(f"{os.getcwd()}/bionty/versions/versions.yaml")
+    versions = load_yaml(VERSIONS_FILE_PATH.resolve())
+
+    table = _generate_rich_versions_table(title="Available versions")
+
+    for entity, db_to_version in versions.items():
+        for db, _to_versions_url in db_to_version.items():
+            for _, version_to_url in _to_versions_url.items():
+                for version, url in version_to_url.items():
+                    table.add_row(entity, db, str(version))
+
+    console.print(table)
+
+
+def display_active_versions(
+    version_table: Literal["_current.yaml", "_lndb.yaml", "_local.yaml"]
+) -> None:  # pragma: no cover
+    """Displays all currently set as default entities and versions in a Rich table.
+
+    Args:
+        version_table: One of '_current.yaml', '_lndb.yaml', '_local.yaml'
+    """
+    VERSIONS_FILE_PATH = Path(f"{os.getcwd()}/bionty/versions/{version_table}")
+    versions = load_yaml(VERSIONS_FILE_PATH.resolve())
+
+    table = _generate_rich_versions_table(
+        title=f"Currently used versions in {version_table}"
+    )
+
+    for entity, db_to_version in versions.items():
+        for db, version in db_to_version.items():
+            table.add_row(entity, db, str(version))
+
+    console.print(table)
+
+
+def _generate_rich_versions_table(title: str) -> Table:  # pragma: no cover
+    table = Table(title=title)
+
+    table.add_column("Entity", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Database", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Version", justify="right", style="cyan", no_wrap=True)
+
+    return table
--- tests/test_current_db_version.py
+++ tests/test_current_db_version.py
@@ -1,6 +1,6 @@
-from bionty.dev import current_db_version
+from bionty.dev import latest_db_version
 
 
 def test_current_db_version():
-    v = current_db_version(db="ensembl")
+    v = latest_db_version(db="ensembl")
     assert v.startswith("release-")
