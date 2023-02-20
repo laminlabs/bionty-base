6a77b37db2d1b8538855be6c2f74251c71bb8757
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-20 05:28:34 +0100

    🔥 No downloading upon import (#205)
    * :fire: No downloading upon import
    
    * :green_heart: Fix tests

--- tests/test_lookup.py
+++ tests/test_lookup.py
@@ -4,4 +4,3 @@ from bionty import lookup
 def test_lookup():
     assert lookup.gene_id.ensembl_gene_id == "ensembl_gene_id"
     assert lookup.protein_id.uniprotkb_id == "uniprotkb_id"
-    assert lookup.species.mouse == "mouse"
