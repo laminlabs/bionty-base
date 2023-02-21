5fa365d7d39c4b57e6de5bb3240023be749a0478
Lukas Heumos <lukas.heumos@posteo.net>
2023-02-21 16:50:51 +0100

    :sparkles: Add HCA ontology (#209)
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- bionty/_celltype/_core.py
+++ bionty/_celltype/_core.py
@@ -7,26 +7,45 @@ from .._ontology import Ontology
 from .._settings import settings
 from .._table import EntityTable
 
+FILENAMES = {
+    "human_cl_ontology": "human_cl_lookup.parquet",
+    "human_ca": "human_ca_lookup.parquet",
+}
+
 
 class CellType(EntityTable):
-    """Cell type.
+    """Cell type ontologies.
 
+    1. Cell ontology
     Edits of terms are coordinated and reviewed on:
     https://github.com/obophenotype/cell-ontology
+
+    2. Human cell atlas ontology
+    Edits of terms are coordinated and reviewed on:
+    https://github.com/HumanCellAtlas/ontology
     """
 
     def __init__(
         self,
         id: str = "ontology_id",
+        species: str = "human",
         database: Optional[str] = None,
         version: Optional[str] = None,
     ) -> None:
         super().__init__(id=id, database=database, version=version)
+        self._species = species
+
+    @property
+    def species(self):
+        """The `name` of `Species` entity EntityTable."""
+        return self._species
 
     @cached_property
     def df(self) -> pd.DataFrame:
         """DataFrame."""
-        self._filepath = settings.datasetdir / "celltype_lookup.parquet"
+        self._filepath = settings.datasetdir / FILENAMES.get(
+            f"{self.species}_{self.database}"
+        )
 
         if not self._filepath.exists():
             df = self._ontology_to_df(self.ontology)
