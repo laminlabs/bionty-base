63732de1b294a8f8aa673ba740684995c840a024
zethson <lukas.heumos@posteo.net>
2023-02-27 10:18:58 +0100

    :bug: Add full nox
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,13 @@
 import nox
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_dir
-from laminci.nox import build_docs, run_pre_commit, run_pytest
+from laminci.nox import (
+    build_docs,
+    login_testuser1,
+    login_testuser2,
+    run_pre_commit,
+    run_pytest,
+    setup_test_instances_from_main_branch,
+)
 
 nox.options.reuse_existing_virtualenvs = True
 
@@ -12,6 +19,9 @@ def lint(session: nox.Session) -> None:
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def build(session):
+    login_testuser1(session)
+    login_testuser2(session)
+    setup_test_instances_from_main_branch(session)
     session.install(".[dev,test]")
     run_pytest(session)
     build_docs(session)
