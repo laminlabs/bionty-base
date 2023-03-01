81a4947fdc720d7c26c31e1b5792cf8c51f1a07d
zethson <lukas.heumos@posteo.net>
2023-03-01 16:56:47 +0100

    :bug: Readd species to normalize
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- bionty/_normalize.py
+++ bionty/_normalize.py
@@ -31,11 +31,11 @@ class NormalizeColumns:
     """Standardizing column names."""
 
     @staticmethod
-    def gene(df: pd.DataFrame):
+    def gene(df: pd.DataFrame, species=None):
         """Column names of gene Entity."""
         df.rename(columns=GENE_COLUMNS, inplace=True)
 
     @staticmethod
-    def protein(df: pd.DataFrame):
+    def protein(df: pd.DataFrame, species=None):
         """Column names of protein Entity."""
         df.rename(columns=PROTEIN_COLUMNS, inplace=True)
