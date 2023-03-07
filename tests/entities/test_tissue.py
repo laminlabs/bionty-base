701255aa535346bbbd5c0f72600e9f4e0f0e52c4
zethson <lukas.heumos@posteo.net>
2023-03-07 12:27:49 +0100

    :bug: Fix tissue test
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/entities/test_tissue.py
+++ tests/entities/test_tissue.py
@@ -10,10 +10,10 @@ def test_uberon_tissue_curation_ontology_id():
             "UBERON:0000005",
             "UBERON:8600001",
             "UBERON:8600002",
-            "This cell line does not exist",
+            "This tissue does not exist",
         ]
     )
-    curated_df = bt.Tissue().curate(df)
+    curated_df = bt.Tissue(database="uberon", version="2023-02-14").curate(df)
 
     curation = curated_df["__curated__"].reset_index(drop=True)
     expected_series = pd.Series([True, True, True, True, False])
@@ -21,19 +21,21 @@ def test_uberon_tissue_curation_ontology_id():
     assert curation.equals(expected_series)
 
 
-# def test_uberon_tissue_curation_name():
-#     df = pd.DataFrame(
-#         index=[
-#             "DA05702 cell",
-#             "DA05703 cell",
-#             "immortal astrocyte cell line cell",
-#             "15P-1 cell",
-#             "This cell line does not exist",
-#         ]
-#     )
-#     curated_df = bt.Tissue(id="name").curate(df)
-#
-#     curation = curated_df["__curated__"].reset_index(drop=True)
-#     expected_series = pd.Series([True, True, True, True, False])
-#
-#     assert curation.equals(expected_series)
+def test_uberon_tissue_curation_name():
+    df = pd.DataFrame(
+        index=[
+            "nose",
+            "chemosensory organ",
+            "epithelium of lobular bronchiole",
+            "smooth muscle tissue of lobular bronchiole",
+            "This tissue does not exist",
+        ]
+    )
+    curated_df = bt.Tissue(id="name", database="uberon", version="2023-02-14").curate(
+        df
+    )
+
+    curation = curated_df["__curated__"].reset_index(drop=True)
+    expected_series = pd.Series([True, True, True, True, False])
+
+    assert curation.equals(expected_series)
