4e6e95eb57c5aa7685120afbee882fae077ede10
Alex Wolf <f.alexander.wolf@gmail.com>
2023-03-09 17:24:01 +0100

    :construction_worker: Simplify nox

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,6 @@
 import nox
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_dir
-from laminci.nox import (
-    build_docs,
-    login_testuser1,
-    login_testuser2,
-    run_pre_commit,
-    run_pytest,
-    setup_test_instances_from_main_branch,
-)
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
 nox.options.reuse_existing_virtualenvs = True
 
@@ -20,8 +13,6 @@ def lint(session: nox.Session) -> None:
 @nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def build(session):
     login_testuser1(session)
-    login_testuser2(session)
-    setup_test_instances_from_main_branch(session)
     session.install(".[dev,test]")
     run_pytest(session)
     build_docs(session)
