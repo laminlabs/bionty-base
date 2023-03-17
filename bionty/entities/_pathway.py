fde70d3727aa9b9287bcb232a190405c0f428af3
zethson <lukas.heumos@posteo.net>
2023-03-17 14:04:28 +0100

    :sparkles: Add pathway ontology
    Signed-off-by: zethson <lukas.heumos@posteo.net>

---
+++ bionty/entities/_pathway.py
@@ -0,0 +1,31 @@
+from typing import Literal, Optional
+
+from .._entity import Entity
+from ._shared_docstrings import _doc_params, doc_entites
+
+
+@_doc_params(doc_entities=doc_entites)
+class Pathway(Entity):
+    """Pathway.
+
+    1. Pathway Ontology
+    Edits of terms are coordinated and reviewed on:
+    https://hpo.jax.org/app/
+
+    Args:
+        {doc_entities}
+    """
+
+    def __init__(
+        self,
+        species: str = "human",
+        id: str = "ontology_id",
+        database: Optional[Literal["pw"]] = None,
+        version: Optional[str] = None,
+    ) -> None:
+        super().__init__(
+            id=id,
+            database=database,
+            version=version,
+            species=species,
+        )
