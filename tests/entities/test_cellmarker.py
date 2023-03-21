4c11cb82618d3eaa113d747f4e8a7469c94944da
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-03-21 20:53:21 +0800

    :bento: Updated CellMarker assets

--- tests/entities/test_cellmarker.py
+++ tests/entities/test_cellmarker.py
@@ -3,7 +3,7 @@ import pandas as pd
 import bionty as bt
 
 
-def test_uniprot_cellmarker_curation_ontology_id():
+def test_human_cellmarker_curation_name():
     df = pd.DataFrame(
         index=["CCR7", "CD69", "CD8A", "CD45RA", "This protein does not exist"]
     )
@@ -16,19 +16,14 @@ def test_uniprot_cellmarker_curation_ontology_id():
     assert curation.equals(expected_series)
 
 
-def test_uniprot_cellmarker_curation_name():
+def test_mouse_cellmarker_curation_name():
     df = pd.DataFrame(
-        index=[
-            "1-MAR",
-            "160 KDA NEUROFILAMENT MEDIUM",
-            "VON WILLEBRAND FACTOR",
-            "TYPE IV COLLAGEN",
-            "This protein does not exist",
-        ]
+        index=["Tcf4", "Cd36", "Cd34", "Cd45", "This protein does not exist"]
     )
-    curated_df = bt.CellMarker(id="name", database="cellmarker", version="2.0").curate(
-        df
-    )
+
+    curated_df = bt.CellMarker(
+        species="mouse", database="cellmarker", version="2.0"
+    ).curate(df)
 
     curation = curated_df["__curated__"].reset_index(drop=True)
     expected_series = pd.Series([True, True, True, True, False])
