156850216abb8471fc9540a5fa7c908396feebfd
zethson <lukas.heumos@posteo.net>
2023-04-13 17:38:50 +0200

    :art: Do not lint with multiple python versions
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
