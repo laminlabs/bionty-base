94651198bf775d483dad35fda4489f4c2c0a062a
Lukas Heumos <lukas.heumos@posteo.net>
2023-04-26 14:54:27 -0700

    Fix pathway ontology (#329)
    * :sparkles: Add new pathway ontology
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :pencil2: Fix pathway entity
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    ---------
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/entities/test_pathway.py
+++ tests/entities/test_pathway.py
@@ -19,3 +19,23 @@ def test_pw_pathway_curation_ontology_id():
     expected_series = pd.Series([True, True, True, True, False])
 
     assert curation.equals(expected_series)
+
+
+def test_pw_pathway_curation_name():
+    df = pd.DataFrame(
+        index=[
+            "Toll-like receptor 9 signaling pathway",
+            "Toll-like receptor TLR1:TLR2 signaling pathway",
+            "classic metabolic pathway",
+            "regulatory pathway",
+            "This pathway does not exist",
+        ]
+    )
+    curated_df = bt.Pathway(database="pw", version="7.74").curate(
+        df, reference_id="name"
+    )
+
+    curation = curated_df["__curated__"].reset_index(drop=True)
+    expected_series = pd.Series([True, True, True, True, False])
+
+    assert curation.equals(expected_series)
