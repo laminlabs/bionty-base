6a869ba33f60cd4086c2a84fa2e20398b3da7fe2
Lukas Heumos <lukas.heumos@posteo.net>
2023-05-02 01:17:16 -0700

    Improve print versions (#336)
    * Return DFs option for print_versions
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Add docstring
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * :art: Improve compatibility
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    ---------
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- bionty/_display_versions.py
+++ bionty/_display_versions.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
+from typing import Optional
 
+import pandas as pd
 from rich.console import Console
 from rich.table import Table
 
@@ -12,8 +14,14 @@ console = Console()
 ROOT_DIR = Path(__file__).parent.resolve()
 
 
-def display_available_versions() -> None:  # pragma: no cover
-    """Displays all available entities and versions in a Rich table."""
+def display_available_versions(
+    return_df: bool = False,
+) -> Optional[pd.DataFrame]:  # pragma: no cover
+    """Displays all available entities and versions in a Rich table.
+
+    Args:
+        return_df: Whether to return a Pandas DataFrame containing the available versions. Defaults to False.
+    """
     VERSIONS_FILE_PATH = (settings.versionsdir / "local.yaml").resolve()
     versions = load_yaml(VERSIONS_FILE_PATH)
 
@@ -25,6 +33,7 @@ def display_available_versions() -> None:  # pragma: no cover
     table.add_column("Database key", justify="right", style="cyan", no_wrap=True)
     table.add_column("All versions", justify="right", style="cyan", no_wrap=True)
 
+    df_rows = []
     for entity, db_to_version in versions.items():
         if entity == "version":
             continue
@@ -32,15 +41,42 @@ def display_available_versions() -> None:  # pragma: no cover
             versions = ""
             _ontology_name = _to_versions_url["name"]
             _ontology_url = _to_versions_url["website"]
-            for version_str, url in _to_versions_url["versions"].items():
+            for version_str, url_md5 in _to_versions_url["versions"].items():
                 versions += str(version_str) + "\n"
-            table.add_row(_ontology_name, _ontology_url, entity, db, versions)
+
+                # Compatibility code for old local.yml files that may not yet have md5s
+                if len(url_md5) > 1:
+                    url = url_md5[0]
+                else:
+                    url = url_md5
+
+                df_rows.append(
+                    {
+                        "Ontology": _ontology_name,
+                        "URL": url,
+                        "Bionty Entity": entity,
+                        "Database key": db,
+                        "All versions": str(version_str),
+                    }
+                )
+                table.add_row(_ontology_name, _ontology_url, entity, db, versions)
+
+    if return_df:
+        df = pd.DataFrame(df_rows)
+        return df
 
     console.print(table)
+    return None
 
 
-def display_active_versions() -> None:  # pragma: no cover
-    """Displays all currently set as default entities and versions in a Rich table."""
+def display_active_versions(
+    return_df: bool = False,
+) -> Optional[pd.DataFrame]:  # pragma: no cover
+    """Displays all currently set as default entities and versions in a Rich table.
+
+    Args:
+        return_df: Whether to return a Pandas DataFrame containing the available versions. Defaults to False.
+    """
     version_table = (
         "._lndb.yaml" if os.getenv("LAMINDB_INSTANCE_LOADED") == 1 else "._current.yaml"
     )
@@ -54,8 +90,15 @@ def display_active_versions() -> None:  # pragma: no cover
     table.add_column("Database", justify="right", style="cyan", no_wrap=True)
     table.add_column("Version", justify="right", style="cyan", no_wrap=True)
 
+    df_rows = []
     for entity, db_to_version in versions.items():
         for db, version in db_to_version.items():
+            df_rows.append({"Entity": entity, "Database": db, "Version": str(version)})
             table.add_row(entity, db, str(version))
 
+    if return_df:
+        df = pd.DataFrame(df_rows)
+        return df
+
     console.print(table)
+    return None
