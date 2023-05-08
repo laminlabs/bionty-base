51f70ad1579f8b2a0b4cd5e464a780e496dbbe5a
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-05-08 15:13:03 +0200

    :sparkles: Added synonyms for CellMarker

--- bionty/entities/_cellmarker.py
+++ bionty/entities/_cellmarker.py
@@ -5,6 +5,8 @@ import pandas as pd
 from .._entity import Entity
 from .._settings import s3_bionty_assets
 
+ALIAS_DICT = {"name": "synonyms"}
+
 
 class CellMarker(Entity):
     """Cell markers.
@@ -27,7 +29,7 @@ class CellMarker(Entity):
             database=database, version=version, species=species, reference_id="name"
         )
 
-    def df(self):
+    def df(self) -> pd.DataFrame:
         """DataFrame.
 
         See ingestion: https://lamin.ai/docs/bionty-assets/ingest/cell-marker-2.0
@@ -36,3 +38,48 @@ class CellMarker(Entity):
         df = pd.read_parquet(localpath)
 
         return df
+
+    def curate(  # type: ignore
+        self,
+        df: pd.DataFrame,
+        column: str = None,
+        reference_id: str = "name",
+    ) -> pd.DataFrame:
+        """Curate index of passed DataFrame to conform with default identifier.
+
+        - If `column` is `None`, checks the existing index for compliance with
+          the default identifier.
+        - If `column` denotes an entity identifier, tries to map that identifier
+          to the default identifier.
+
+        Returns the DataFrame with the curated index and a boolean `__curated__`
+        column that indicates compliance with the default identifier.
+
+        In addition to the .curate() in base class, this also performs alias mapping.
+        """
+        agg_col = ALIAS_DICT.get(reference_id)
+        df = df.copy()
+
+        # if the query column name does not match any columns in the self.df()
+        # Bionty assume the query column and the self._id_field uses the same type of
+        # identifier
+        orig_column = column
+        if column is not None and column not in self.df().columns:
+            # normalize the identifier column
+            if column in df.columns:
+                raise ValueError("{column_norm} column already exist!")
+            else:
+                column = reference_id if column is None else column
+                df.rename(columns={orig_column: column}, inplace=True)
+            agg_col = ALIAS_DICT.get(column)
+
+        return (
+            super()
+            ._curate(
+                df=df,
+                column=column,
+                agg_col=agg_col,
+                reference_id=reference_id,
+            )
+            .rename(columns={column: orig_column})
+        )
