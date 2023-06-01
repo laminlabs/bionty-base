d5f04f1dd122b6df4911d9958a470a1cefbaf34d
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-01 12:17:38 +0200

    👷 Remove nox venv (#388)
    * :construction_worker: Remove nox venv
    
    * :green_heart: Fix
    
    * :green_heart: Fix
    
    * :pencil2: Typo
    
    * :green_heart: Fix
    
    * :green_heart: Fix
    
    * :green_heart: Try resolving version conflict

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
 import nox
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
-nox.options.reuse_existing_virtualenvs = True
+nox.options.default_venv_backend = "none"
 
 
 @nox.session
@@ -15,8 +16,7 @@ def lint(session: nox.Session) -> None:
 @nox.session
 @nox.parametrize("package", ["bionty", "lnschema-bionty"])
 def build(session, package):
-    login_testuser1(session)
-    session.install(".[dev,test]")
+    session.run(*"pip install .[dev,test]".split())
     if package == "bionty":
         run_pytest(session)
         build_docs(session)
@@ -25,7 +25,12 @@ def build(session, package):
     else:
         # navigate into submodule so that lamin-project.yml is correctly read
         os.chdir("./lnschema-bionty")
-        session.install(".[test]")
+        session.run(*"pip install .[test]".split())
+        session.run(*"git clone https://github.com/laminlabs/lamindb --depth 1".split())
+        if sys.platform.startswith("linux"):  # remove version pin when running on CI
+            session.run(*"sed -i /lnschema_bionty/d ./lamindb/pyproject.toml".split())
+        session.run(*"pip install ./lamindb".split())
+        login_testuser1(session)
         session.run(
             "lamin",
             "init",
