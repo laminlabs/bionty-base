81614f333511afd693f4adc5940ce5809e36b5f5
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-07 02:58:29 -0400

    🧪 Add tests to versions file generation (#397)
    * :test_tube: Add tests to versions file generation
    
    * :label: Return types
    
    * :art: Parse_versions_yaml to always return df

--- tests/dev/test_handle_versions.py
+++ tests/dev/test_handle_versions.py
@@ -3,7 +3,13 @@ import tempfile
 
 import pytest
 
-# from unittest.mock import patch
+from bionty.dev._handle_versions import (
+    add_records_to_existing_dict,
+    parse_current_versions,
+    parse_versions_yaml,
+    records_diff_btw_yamls,
+)
+from bionty.dev._io import load_yaml
 
 
 @pytest.fixture(scope="function")
@@ -12,28 +18,87 @@ def versions_yaml_replica():
     version: "0.2.0"
     Species:
       ensembl:
-        versions:
+        all:
           release-108:
-            source: https://ftp.ensembl.org/pub/release-108/mysql/
-            md5: ""
-        species:
-          - all
+            source: https://ftp.ensembl.org/pub/release-108/species_EnsemblVertebrates.txt
         name: Ensembl
         website: https://www.ensembl.org/index.html
     Gene:
       ensembl:
-        versions:
+        human:
           release-108:
-            source: https://ftp.ensembl.org/pub/release-108/mysql/
-            md5: ""
+            source: https://ftp.ensembl.org/pub/release-108/mysql/homo_sapiens_core_108_38/
           release-107:
-            source: https://ftp.ensembl.org/pub/release-107/mysql/
-            md5: ""
-        species:
-          - human
-          - mouse
+            source: https://ftp.ensembl.org/pub/release-107/mysql/homo_sapiens_core_107_38/
+        mouse:
+          release-108:
+            source: https://ftp.ensembl.org/pub/release-108/mysql/mus_musculus_core_108_39/
         name: Ensembl
         website: https://www.ensembl.org/index.html
+    CellType:
+      cl:
+        name: Cell Ontology
+        website: https://obophenotype.github.io/cell-ontology/
+        all:
+          2023-02-15:
+            source: http://purl.obolibrary.org/obo/cl/releases/2023-02-15/cl-base.owl
+            md5: 9331a6a029cb1863bd0584ab41508df7
+          2022-08-16:
+            source: http://purl.obolibrary.org/obo/cl/releases/2022-08-16/cl.owl
+            md5: d0655766574e63f3fe5ed56d3c030880
+    """
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as f:
+        f.write(input_file_content)
+        f.flush()
+        yield f.name
+
+    os.unlink(f.name)
+
+
+@pytest.fixture(scope="function")
+def new_versions_yaml_replica():
+    input_file_content = """
+    version: "0.2.0"
+    Species:
+      ensembl:
+        all:
+          release-108:
+            source: https://ftp.ensembl.org/pub/release-108/species_EnsemblVertebrates.txt
+        name: Ensembl
+        website: https://www.ensembl.org/index.html
+        new-species:
+          release-x:
+            source: new-species-source-link
+    Gene:
+      ensembl:
+        human:
+          release-108:
+            source: https://ftp.ensembl.org/pub/release-108/mysql/homo_sapiens_core_108_38/
+          release-107:
+            source: https://ftp.ensembl.org/pub/release-107/mysql/homo_sapiens_core_107_38/
+        mouse:
+          release-108:
+            source: https://ftp.ensembl.org/pub/release-108/mysql/mus_musculus_core_108_39/
+        name: Ensembl
+        website: https://www.ensembl.org/index.html
+      new-source:
+        human:
+          release-x:
+            source: new-gene-source-link
+    CellType:
+      cl:
+        name: Cell Ontology
+        website: https://obophenotype.github.io/cell-ontology/
+        all:
+          new-version:
+            source: new-cell-type-source
+            md5: new-md5
+          2023-02-15:
+            source: http://purl.obolibrary.org/obo/cl/releases/2023-02-15/cl-base.owl
+            md5: 9331a6a029cb1863bd0584ab41508df7
+          2022-08-16:
+            source: http://purl.obolibrary.org/obo/cl/releases/2022-08-16/cl.owl
+            md5: d0655766574e63f3fe5ed56d3c030880
     """
     with tempfile.NamedTemporaryFile(mode="w+", delete=False) as f:
         f.write(input_file_content)
@@ -58,27 +123,84 @@ def current_yaml_replica():
     os.unlink(f.name)
 
 
-# def test_get_missing_defaults(versions_yaml_replica, current_yaml_replica):
-#     with patch.multiple(
-#         "bionty.dev._handle_versions",
-#         VERSIONS_PATH=versions_yaml_replica,
-#         _CURRENT_PATH=current_yaml_replica,
-#     ):
-#         expected = [
-#             (
-#                 "Gene",
-#                 "ensembl",
-#                 "human",
-#                 "release-108",
-#             ),
-#             (
-#                 "Gene",
-#                 "ensembl",
-#                 "mouse",
-#                 "release-108",
-#             ),
-#         ]
-#         from bionty.dev._handle_versions import _get_missing_defaults
+def test_parse_versions_yaml(versions_yaml_replica):
+    parsed_df = parse_versions_yaml(versions_yaml_replica)
+    assert parsed_df.shape == (6, 8)
+    assert all(
+        parsed_df["entity"].values
+        == ["Species", "Gene", "Gene", "Gene", "CellType", "CellType"]
+    )
+    assert all(
+        parsed_df["species"].values == ["all", "human", "human", "mouse", "all", "all"]
+    )
+    assert all(
+        parsed_df["source_key"].values
+        == ["ensembl", "ensembl", "ensembl", "ensembl", "cl", "cl"]
+    )
 
-#         result = _get_missing_defaults(source="versions")
-#         assert result == expected
+
+def test_parse_current_versions(versions_yaml_replica):
+    expected = {
+        "Species": {"all": {"ensembl": "release-108"}},
+        "Gene": {
+            "human": {"ensembl": "release-108"},
+            "mouse": {"ensembl": "release-108"},
+        },
+        "CellType": {"all": {"cl": "2023-02-15"}},
+    }
+
+    assert parse_current_versions(versions_yaml_replica) == expected
+
+
+def test_add_records_to_existing_dict(new_versions_yaml_replica, versions_yaml_replica):
+    expected = [
+        {
+            "entity": "Species",
+            "source_key": "ensembl",
+            "species": "new-species",
+            "version": "release-x",
+            "url": "new-species-source-link",
+            "md5": "",
+            "source_name": "Ensembl",
+            "source_website": "https://www.ensembl.org/index.html",
+        },
+        {
+            "entity": "Gene",
+            "source_key": "new-source",
+            "species": "human",
+            "version": "release-x",
+            "url": "new-gene-source-link",
+            "md5": "",
+            "source_name": "",
+            "source_website": "",
+        },
+        {
+            "entity": "CellType",
+            "source_key": "cl",
+            "species": "all",
+            "version": "new-version",
+            "url": "new-cell-type-source",
+            "md5": "new-md5",
+            "source_name": "Cell Ontology",
+            "source_website": "https://obophenotype.github.io/cell-ontology/",
+        },
+    ]
+
+    records = records_diff_btw_yamls(new_versions_yaml_replica, versions_yaml_replica)
+
+    assert records == expected
+
+    updated_dict = add_records_to_existing_dict(
+        records, load_yaml(versions_yaml_replica)
+    )
+    assert updated_dict.get("Species").get("ensembl").get("new-species").get(
+        "release-x"
+    ) == {"source": "new-species-source-link", "md5": ""}
+    assert updated_dict.get("Gene").get("new-source").get("human").get("release-x") == {
+        "source": "new-gene-source-link",
+        "md5": "",
+    }
+    assert updated_dict.get("CellType").get("cl").get("all").get("new-version") == {
+        "source": "new-cell-type-source",
+        "md5": "new-md5",
+    }
