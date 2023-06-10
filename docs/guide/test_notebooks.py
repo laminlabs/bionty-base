a39949bdfc1bc720712f79383dda8188fc0b3115
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-10 12:52:28 +0200

    :white_check_mark: Fix notebook execution

--- docs/guide/test_notebooks.py
+++ docs/guide/test_notebooks.py
@@ -1,16 +1,8 @@
 from pathlib import Path
 
 import nbproject_test as test
-from lamin_logger import logger
 
 
 def test_notebooks():
-    # assuming this is in the tests folder
-    docs_folder = Path(__file__).parents[1] / "docs/"
-
-    for check_folder in docs_folder.glob("./**"):
-        # these are the typical notebook testpaths
-        if not str(check_folder).endswith(("guide", "faq")):
-            continue
-        logger.info(f"\n---{check_folder.stem}---")
-        test.execute_notebooks(check_folder, write=True)
+    nbdir = Path(__file__).parent
+    test.execute_notebooks(nbdir, write=True)
