bb8b48c449f94b8095a93dca71875d02d8eadf7a
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-16 16:48:33 +0200

    💚 Fix

--- tests/test_bionty.py
+++ tests/test_bionty.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 
 import bionty as bt
+from bionty.dev._handle_sources import CURRENT_SOURCES, LAMINDB_SOURCES
 
 
 def test_unavailable_sources():
@@ -10,10 +11,13 @@ def test_unavailable_sources():
         bt.CellType(source="random")
 
 
+@pytest.fixture(scope="module")
 def test_loaded_lamindb():
     lnenv = Path.home() / ".lamin/current_instance.env"
     open(lnenv, "a").close()
     ct = bt.CellType(version="2022-08-16")
     assert ct.source is None
     assert str(ct) == "invalid Bionty object"
-    lnenv.unlink()
+    import shutil
+
+    shutil.copyfile(CURRENT_SOURCES, LAMINDB_SOURCES)
