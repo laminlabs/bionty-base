d03f43c616697a08a9b604160e6851643f4cab07
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-16 17:16:57 +0200

    💚 Added back reset_sources

--- tests/test_lamindb.py
+++ tests/test_lamindb.py
@@ -7,7 +7,7 @@ from bionty.dev._handle_sources import CURRENT_SOURCES, LAMINDB_SOURCES
 def test_loaded_lamindb():
     import shutil
 
-    shutil.copyfile(CURRENT_SOURCES, LAMINDB_SOURCES)
+    shutil.copyfile(CURRENT_SOURCES.as_posix(), LAMINDB_SOURCES.as_posix())
 
     lnenv = Path.home() / ".lamin/current_instance.env"
     open(lnenv, "a").close()
