44c967f141a31cae516fe690856637d1fc2982a3
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-09 11:40:18 +0200

    📝 Build docs standalone again

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,5 @@
-import shutil
-
 import nox
-from laminci import upload_docs_artifact
+from laminci import move_built_docs_to_docs_slash_project_slug
 from laminci.nox import build_docs, run_pre_commit
 
 nox.options.default_venv_backend = "none"
@@ -21,6 +19,5 @@ def build(session, group):
         session.run(*f"pytest {coverage_args} ./tests".split())
     elif group == "bionty-docs":
         session.run(*f"pytest -s {coverage_args} ./docs/guide".split())
-        shutil.copy("README.md", "docs/README.md")
         build_docs(session)
-        upload_docs_artifact(aws=True)
+        move_built_docs_to_docs_slash_project_slug()
