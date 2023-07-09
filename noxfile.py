fa91cbccfc133744bd1093c1860c201763cf3032
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-09 12:32:51 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -11,18 +11,18 @@ def lint(session: nox.Session) -> None:
 
 
 @nox.session
-def docs(session):
+def docs(session: nox.Session):
     build_docs(session, strict=True)
 
 
 @nox.session
 @nox.parametrize("group", ["bionty-unit", "bionty-docs"])
-def build(session, group):
+def build(session: nox.Session, group: str):
     session.run(*"pip install -e .[dev]".split())
     coverage_args = "--cov=bionty --cov-append --cov-report=term-missing"  # noqa
     if group == "bionty-unit":
         session.run(*f"pytest {coverage_args} ./tests".split())
     elif group == "bionty-docs":
         session.run(*f"pytest -s {coverage_args} ./docs/guide".split())
-        docs()
+        docs(session)
         move_built_docs_to_docs_slash_project_slug()
