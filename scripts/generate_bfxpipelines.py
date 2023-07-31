b148e3e84fc9be6efc1bb5c2b8fec8d45dc60d35
zethson <lukas.heumos@posteo.net>
2023-07-31 13:40:21 +0200

    :sparkles: Add bfxpipelines json files

--- scripts/generate_bfxpipelines.py
+++ scripts/generate_bfxpipelines.py
@@ -10,7 +10,7 @@ from typing import Dict, Optional
 from github import Github
 from rich.progress import track
 
-BASE_BFX_PIPELINES_PATH = "bionty/versions/bfxpipelines_data"
+BASE_BFX_PIPELINES_PATH = "./scripts/pipelines_info"
 
 
 def base62(n_char: int) -> str:
