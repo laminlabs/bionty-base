9623ce117acb0088b1ade8e047c2a5f154519051
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-09-13 10:33:57 +0200

    💚 Fix

--- tests/entities/test_biosample.py
+++ tests/entities/test_biosample.py
@@ -4,4 +4,4 @@ import bionty as bt
 def test_ncbi_biosample():
     bs = bt.BioSample(source="ncbi")
     df = bs.df()
-    assert "edta_inhibitor_tested" in df.abbr
+    assert "edta_inhibitor_tested" in df.abbr.tolist()
