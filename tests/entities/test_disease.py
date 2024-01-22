9eef0df19679426e5fb44065c9f903eabaf7fe38
zethson <lukas.heumos@posteo.net>
2024-01-22 12:41:47 +0100

    :sparkles: Add ICD 9 + 10
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/entities/test_disease.py
+++ tests/entities/test_disease.py
@@ -40,3 +40,43 @@ def test_doid_disease_inspect_ontology_id():
     expected_series = pd.Series([True, True, True, True, False])
 
     assert inspect.equals(expected_series)
+
+
+def test_icd_9_disease_inspect_name():
+    df = pd.DataFrame(
+        index=[
+            "Cholera d/t vib cholerae",
+            "Typhoid fever",
+            "Mult gest-plac/sac NOS",
+            "Paratyphoid fever a",
+            "This disease does not exist",
+        ]
+    )
+
+    ds = bt.Disease(source="icd", version="icd-9-2011")
+    inspected_df = ds.inspect(df.index, field=ds.name, return_df=True)
+
+    inspect = inspected_df["__validated__"].reset_index(drop=True)
+    expected_series = pd.Series([True, True, True, True, False])
+
+    assert inspect.equals(expected_series)
+
+
+def test_icd_10_disease_inspect_name():
+    df = pd.DataFrame(
+        index=[
+            "Vaping-related disorder",
+            "COVID-19",
+            "Typhoid fever with heart involvement",
+            "Typhoid fever, unspecified",
+            "This disease does not exist",
+        ]
+    )
+
+    ds = bt.Disease(source="icd", version="icd-10-2020")
+    inspected_df = ds.inspect(df.index, field=ds.name, return_df=True)
+
+    inspect = inspected_df["__validated__"].reset_index(drop=True)
+    expected_series = pd.Series([True, True, True, True, False])
+
+    assert inspect.equals(expected_series)
