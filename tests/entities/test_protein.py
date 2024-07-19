38786ed4081a491521a377ded2f74e7e2dae1f8d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-07-19 11:23:17 +0200

    ✨ New ontology: Protein-uniprot-2024-03 (#582)
    * ✨ New ontology: Protein, 2024-03
    
    * 💚 Fix tests
    
    * 💚 Fix docs
    
    * 💚 Fix docs

--- tests/entities/test_protein.py
+++ tests/entities/test_protein.py
@@ -5,10 +5,10 @@ import pandas as pd
 def test_uniprot_protein_inspect_uniprotkb_id():
     df = pd.DataFrame(
         index=[
-            "A0A024QZ08",
+            "A0A024QZ08",  # no longer exist in 2024-03 version
             "X6RLV5",
             "X6RM24",
-            "A0A024QZQ1",
+            "A0A024QZQ1",  # no longer exist in 2024-03 version
             "This protein does not exist",
         ]
     )
@@ -17,6 +17,6 @@ def test_uniprot_protein_inspect_uniprotkb_id():
     inspected_df = pr.inspect(df.index, pr.uniprotkb_id, return_df=True)
 
     inspect = inspected_df["__validated__"].reset_index(drop=True)
-    expected_series = pd.Series([True, True, True, True, False])
+    expected_series = pd.Series([False, True, True, False, False])
 
     assert inspect.equals(expected_series)
